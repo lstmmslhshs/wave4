ad00f6bd604369ad35148d9b62d5776be15f19ad
lstmmslhshs <62666225+lstmmslhshs@users.noreply.github.com>
2020-06-01 23:09:42 -0400

    Add files via upload

---
+++ pig.py
@@ -0,0 +1,15 @@
+from F import Wor
+
+l = str(input("Sentence"))
+
+
+
+x= l.split(" ")
+
+lenth =len(x)
+
+
+
+for num in range (0,lenth):
+
+    print(W(x[num]), end =" ")
---
+++ reverse1.py
@@ -0,0 +1,19 @@
+import Reverse
+
+dictionary_1 = {
+
+    'name': 'Steven',
+
+    'birthday': 20020811,
+
+    'sex': 'M',
+
+    'note': 'M'
+
+
+
+}
+
+
+
+print(Reverse.reverse(dictionary_1, 'M'))
